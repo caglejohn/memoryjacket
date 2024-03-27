e3400fe74f387e6eb01d6fc0328fb9aaa11a79bd
John King Cagle <107167509+caglejohn@users.noreply.github.com>
2024-03-27 11:02:04 -0400

    Added 'onclick' resource

---
+++ Memory Jacket/Assets/Scripts/ClickManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickManager : MonoBehaviour
+{
+    public void ClickReaction()
+    {
+        Debug.Log("TEST");
+    }
+}
