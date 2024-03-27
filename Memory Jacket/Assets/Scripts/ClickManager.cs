8cf57d26a9dcad49df8f607117256032ed3ef84a
John King Cagle <107167509+caglejohn@users.noreply.github.com>
2024-03-27 12:58:26 -0400

    Added movement for the main character, added item system

--- Memory Jacket/Assets/Scripts/ClickManager.cs
+++ Memory Jacket/Assets/Scripts/ClickManager.cs
@@ -1,11 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Progress;
 
 public class ClickManager : MonoBehaviour
 {
-    public void ClickReaction()
+    float moveSpeed = 3.5f, moveAccuracy = 0.15f;
+    public Transform character;
+    public void GoToItem(ItemData item)
     {
-        Debug.Log("TEST");
+        StartCoroutine(MoveToPoint(item.goToPoint.position));
+        TryGettingItem(item);
+    }
+
+    public IEnumerator MoveToPoint(Vector2 point)
+    {
+        Vector2 positionDifference = point - (Vector2)character.position; // Set Direction
+        while(positionDifference.magnitude > moveAccuracy) // Stop when we're close to the point (accuracy)
+        {
+            character.Translate(moveSpeed * positionDifference.normalized * Time.deltaTime); // Move in direction frame after frame
+            positionDifference = point - (Vector2)character.position;
+            yield return null;
+        }
+        
+        character.position = point;
+        yield return null;
+    }
+
+    private void TryGettingItem(ItemData item)
+    {
+        if (item.requiredItemID == -1 || GameManager.collectedItems.Contains(item.requiredItemID))
+        {
+            GameManager.collectedItems.Add(item.itemID);
+            Debug.Log("item collected");
+        }
     }
 }
